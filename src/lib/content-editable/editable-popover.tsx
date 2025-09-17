8f0e525ba00e83298050385bf773173283b20e31
Soroush Jaberi <sorooshjaberi@gmail.com>
2025-09-17 18:01:54 +0330

    fix: cleaner ui

--- src/lib/content-editable/editable-popover.tsx
+++ src/lib/content-editable/editable-popover.tsx
@@ -15,7 +15,7 @@ type Props = {
   changeHandler: (changePath: string, newValue: EditableContentValue) => void;
 };
 
-const POSITION_PRECISION = 10;
+const POSITION_PRECISION = 28;
 
 function getTextStart(el: HTMLElement, prevLeft?: number) {
   const fallbackPosition =
@@ -58,28 +58,35 @@ const EditablePopovers: React.FC<Props> = ({
 
   const updatePositions = () => {
     setPositions((prevPositions) => {
-      const map = new Map(
-        prevPositions.map((position) => [position.el, position.left])
-      );
-
       const tempOccupiedPositions = new Set<string>();
 
       return editableElements.map((el) => {
         let left = getTextStart(el) ?? 0;
         let top = el.getBoundingClientRect().y + window.scrollY;
 
-        while (tempOccupiedPositions.has(getSetKey(left, top))) {
-          top += 40;
-        }
-
-        tempOccupiedPositions.add(getSetKey(left, top));
-
         const type: EditableElementType = el.hasAttribute("data-x-group")
           ? "group"
           : el.parentElement?.hasAttribute("data-x-group")
           ? "item"
           : "field";
-          
+
+        switch (type) {
+          case "group":
+            left -= POSITION_PRECISION * 2;
+            top -= POSITION_PRECISION * 2;
+            break;
+          case "item":
+            left -= POSITION_PRECISION;
+            top -= POSITION_PRECISION;
+            break;
+        }
+
+        // while (tempOccupiedPositions.has(getSetKey(left, top))) {
+        //   top += 1;
+        // }
+
+        tempOccupiedPositions.add(getSetKey(left, top));
+
         return {
           el,
           top,
@@ -164,17 +171,16 @@ const EditablePopovers: React.FC<Props> = ({
     positions.map(({ el, top, left, type }, i) => (
       <div
         key={`editable-popover-${i}`}
-        className="absolute z-50 flex flex-col gap-1"
+        className="absolute z-40 flex flex-col gap-1 opacity-60"
         style={{
-          top,
-          left: left > 30 ? left - 30 : left,
-          transform: "translateX(-50%)",
+          left: left > POSITION_PRECISION ? left - POSITION_PRECISION : left,
+          top: top > POSITION_PRECISION / 2 ? top - POSITION_PRECISION / 2 : top,
         }}
       >
         {type === "field" && (
           <button
             onClick={() => handlePopoverClick(el, type)}
-            className="w-7 h-7 rounded-full bg-yellow-200 shadow-md hover:bg-yellow-300 transition"
+            className="w-7 h-7 rounded-full bg-yellow-200 hover:bg-yellow-300 transition"
           >
             ✏️
           </button>
@@ -182,7 +188,7 @@ const EditablePopovers: React.FC<Props> = ({
         {type === "item" && (el.parentElement?.childElementCount ?? 0) > 1 && (
           <button
             onClick={() => handlePopoverClick(el, type)}
-            className="w-7 h-7 rounded-full bg-red-200 shadow-md hover:bg-red-300 transition"
+            className="w-7 h-7 rounded-full bg-red-200 hover:bg-red-300 transition"
           >
             ❌
           </button>
@@ -190,7 +196,7 @@ const EditablePopovers: React.FC<Props> = ({
         {type === "group" && (
           <button
             onClick={() => handlePopoverClick(el, type)}
-            className="w-7 h-7 rounded-full bg-green-200 shadow-md hover:bg-green-300 transition"
+            className="w-7 h-7 rounded-full bg-green-200 hover:bg-green-300 transition"
           >
             ➕
           </button>
